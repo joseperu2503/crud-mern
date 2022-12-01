eb4805682d73f0e2a5d2d0fdab392ad54bc03792
joseperu2503 <51133263+joseperu2503@users.noreply.github.com>
2022-11-30 19:13:17 -0500

    commit

---
+++ backend/database/db.js
@@ -0,0 +1,8 @@
+import { Sequelize } from "sequelize";
+
+const db = new Sequelize(process.env.DB_DATABASE,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_CONNECTION
+})
+
+export default db
