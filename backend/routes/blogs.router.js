b3946239162a13465c087bd1a872f56c62380bbd
joseperu2503 <51133263+joseperu2503@users.noreply.github.com>
2022-12-05 20:19:37 -0500

    jwt verify

--- backend/routes/blogs.router.js
+++ backend/routes/blogs.router.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const passport = require('passport')
 const BlogController = require("../controllers/BlogController.js");
 const { validatorHandler } = require("../middlewares/validator.handler.js");
 const { createBlogSchema, updateBlogSchema, getBlogSchema } = require("../schemas/blog.schema.js");
 
 const router = express.Router()
 
-router.get('/', BlogController.index)
+router.get('/', passport.authenticate('jwt', {session: false}),BlogController.index)
 router.get('/:id', validatorHandler(getBlogSchema, 'params') ,BlogController.show)
 router.post('/',validatorHandler(createBlogSchema, 'body'), BlogController.store)
 router.put('/:id',validatorHandler(getBlogSchema, 'params'),validatorHandler(updateBlogSchema, 'body'), BlogController.update)
--- backend/utils/auth/index.js
+++ backend/utils/auth/index.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
+const JwtStrategy = require('./strategies/jwt.strategy');
 //Require strategy used
 const localStrategy = require('./strategies/local.strategy');
 //Tell to passport we'll use localStrategy 
 passport.use(localStrategy);
+passport.use(JwtStrategy)
